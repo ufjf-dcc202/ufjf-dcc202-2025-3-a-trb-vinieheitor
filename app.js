0f472535f374969daa64731943b98342f675d523
Heitor Tetzner Pereira <heitortetzner2015@gmail.com>
2026-01-16 11:21:12 -0300

    Diferencia os diferentes tiles

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 let tamanhoTabuleiro = 10;
 let quadradosTabuleiro = [];
 
-iniciarTabuleiro();
+carregarNivel("nivel1.txt");
 
-function iniciarTabuleiro() {
+function iniciarTabuleiro(nivel) {
+    //console.log(nivel);
     let tabuleiroDoc = document.getElementById("tabuleiro");
     let novoTabuleiroHTML = "";
     for (let i = 0; i < tamanhoTabuleiro; i++) {
         novoTabuleiroHTML += "\n<tr class='linha'>"
         for (let j = 0; j < tamanhoTabuleiro; j++) {
-            novoTabuleiroHTML += "\n<td class='quadrado'></td>"
+            let index = (i * 10 + j);
+            let tile = nivel[index];
+            let classes = "quadrado";
+            switch (tile) {
+                case "0":
+                    classes += " plano";
+                    break;
+                case "1":
+                    classes += " parede";
+                    break;
+            }
+            novoTabuleiroHTML += "\n<td class='" + classes + "'></td>"
             let novoQuadrado = {
                 posX : j,
                 posY : i
@@ -19,6 +31,16 @@ function iniciarTabuleiro() {
         novoTabuleiroHTML += "\n</tr>"
     }
     tabuleiroDoc.innerHTML += novoTabuleiroHTML;
+}
+
+function carregarNivel(arquivo) {
+    fetch("nivel1.txt")
+     .then((res) => res.text())
+     .then((text) => {
+        let trimmed_text = text.replace(/\s/g, '');
+        iniciarTabuleiro(trimmed_text.split(''));
+    })
+     .catch((e) => console.error(e));
 }
 
 let robo = {
